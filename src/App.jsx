b020f5def9c480eb1b71876a34bdc0acf7626c5f
Nataliya Kim <magistr1982@gmail.com>
2024-12-11 11:39:01 +0200

    Add colors to tailwind.config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome to Awwwesome</h1>
+      <h1 className="text-5xl text-green-500 font-bold">Welcome to Awwwesome</h1>
     </main>
   )
 }
