d4a4790ec04e09a911825fded919058c5bc5b3a0
Nataliya Kim <magistr1982@gmail.com>
2024-12-12 10:38:17 +0200

    Add small videos to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,27 @@
+import { useRef, useState } from "react";
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const getVideoSrc = (index) => `
+    videos/hero-${index}.mp4
+  `;
   return (
     <section className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -7,8 +30,19 @@ const Hero = () => {
       >
         <div className="">
           <div className="mask-clip-path absolute-center absolute z-50 size-64 overflow-hidden cursor-pointer rounded-lg">
-            <div>
-              MiniVideoPlayer
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(currentIndex + 1)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
             </div>
           </div>
         </div>
