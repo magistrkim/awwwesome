38e6007734d6717f55f4fc0915ee80b0a2bc60c1
Nataliya Kim <magistr1982@gmail.com>
2024-12-12 10:14:42 +0200

    Add styles to index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
-
-
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div className="">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 overflow-hidden cursor-pointer rounded-lg">
+            <div>
+              MiniVideoPlayer
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
