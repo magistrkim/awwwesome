78a948a621689db8dd2b6cfb013e0571813311b7
Nataliya Kim <magistr1982@gmail.com>
2024-12-18 19:41:31 +0200

    Add title to Feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,15 @@
-
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="text-blue-50 font-circular-web text-lg">
+            Into the MetaGame Layer
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Features
+export default Features;
