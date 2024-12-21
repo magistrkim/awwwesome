bbe9edaab1d80566113c7e6f0188fae9b4b309bb
Nataliya Kim <magistr1982@gmail.com>
2024-12-21 13:21:52 +0200

    Complete BentoCard UI

--- src/components/BentoCard.jsx
+++ src/components/BentoCard.jsx
@@ -1,5 +1,31 @@
-const BentoCard = ({ src, title, description, isComingSoon }) => {
-  return <div className="text-white">{title}</div>;
+import PropTypes from "prop-types";
+
+const BentoCard = ({ src, title, description }) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 object-cover size-full object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font"> {title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BentoCard;
+
+BentoCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,26 +8,27 @@ const Features = () => {
           <p className="text-blue-50 font-circular-web text-lg">
             Into the MetaGame Layer
           </p>
+
+          <p className="max-w-md text-blue-50 font-circular-web text-lg opacity-50">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into a interconnected overlay
+            experience on your world.
+          </p>
         </div>
-        <p className="max-w-md text-blue-50 font-circular-web text-lg opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products converge into a interconnected overlay experience on
-          your world.
-        </p>
-      </div>
-      <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
-        <BentoCard
-          src="/videos/feature-1.mp4"
-          title={
-            <>
-              radia<b>n</b>t
-            </>
-          }
-          description="A cross-platform metagame app, 
+
+        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard
+            src="/videos/feature-1.mp4"
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, 
           turning your activities across Web2 and Web3 
           games into a rewarding adventure."
-          isComingSoon={true}
-        />
+          />
+        </div>
       </div>
     </section>
   );
