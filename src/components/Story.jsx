88b44b95170e08e20de3cacec1c0df4364095cce
Nataliya Kim <magistr1982@gmail.com>
2024-12-26 16:50:49 +0200

    Create handleMouse function to rotate the image

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,10 +1,43 @@
 import { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
+import gsap from "gsap";
 
 const Story = () => {
   const frameRef = useRef("null");
-  const handleMouseLeave = () => {};
-  const handleMouseMove = () => {};
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+    if (!element) return;
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    const element = frameRef.current;
+    if (!element) return;
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -40;
+    const rotateY = ((x - centerX) / centerX) * 40;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
