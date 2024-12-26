86385b7dbaa16b71af0220700897801056cff226
Nataliya Kim <magistr1982@gmail.com>
2024-12-26 16:36:18 +0200

    Add AnimatedTitle to Story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,42 @@
-
+import { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
 
 const Story = () => {
+  const frameRef = useRef("null");
+  const handleMouseLeave = () => {};
+  const handleMouseMove = () => {};
   return (
-    <div>Story</div>
-  )
-}
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          The multiversal ip world
+        </p>
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Story
+export default Story;
