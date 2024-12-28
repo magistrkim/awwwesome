6a515318b8aabc91f489820fd96434f4be383927
Nataliya Kim <magistr1982@gmail.com>
2024-12-28 15:37:22 +0200

    Create ImageClipBox component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,15 @@
-
+import ImageClipBox from "./ImageClipBox";
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <section id="contacts" className="my-20 min-h-96 w-screen px-10">
+      <div className="relative rounded-lg text-blue-50 bg-black py-24 sm:overflow-hidden">
+        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96 ">
+          <ImageClipBox />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contacts
+export default Contacts;
---
+++ src/components/ImageClipBox.jsx
@@ -0,0 +1,11 @@
+
+
+const ImageClipBox = () => {
+  return (
+    <div className="contact-clip-path-1">
+      <img src="img/contact-1.webp" alt="" />
+    </div>
+  );
+}
+
+export default ImageClipBox
