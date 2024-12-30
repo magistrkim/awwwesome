0fbdd67551ae5c8ad757fc4ad2b570b41b255b75
Nataliya Kim <magistr1982@gmail.com>
2024-12-30 09:32:53 +0200

    Complete Contacts section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,10 @@
+import Button from "./Button";
 import ImageClipBox from "./ImageClipBox";
 
 const Contacts = () => {
   return (
     <section id="contacts" className="my-20 min-h-96 w-screen px-10">
-      <div className="relative rounded-lg text-blue-50 bg-black py-24 sm:overflow-hidden">
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
             src="/img/contact-1.webp"
@@ -26,10 +27,11 @@ const Contacts = () => {
         </div>
         <div className="flex flex-col items-center text-center">
           <p className="font-general text-[10px] uppercase">Join Zentry</p>
-          <h3 className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]">
+          <h3 className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[5rem]">
             Let&apos;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t
             <b>o</b>gether
           </h3>
+          <Button title="Contact us" containerClass="mt-10 cursor-pointer" />
         </div>
       </div>
     </section>
