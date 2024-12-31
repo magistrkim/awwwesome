c2783f4cf7cc25e0357bbcfc68bf775b26648cca
Nataliya Kim <magistr1982@gmail.com>
2024-12-31 08:30:43 +0200

    Create Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-screen bg-violet-800 py-4 text-white">
+      <div className="container mx-auto flex flex-col justify-between items-center gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">
+          &copy;MagistrKim 2024. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
